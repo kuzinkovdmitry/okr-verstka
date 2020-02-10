cda4d0a81cde2725b2e08ed85e2ef1555179463d
Дмитрий <kuzin_kov@ukr.net>
2020-02-10 18:47:47 +0200

    init-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { IntroComponent } from './intro/intro.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PeoplesComponent } from './peoples/peoples.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    IntroComponent,
+    BenefitsComponent,
+    AboutUsComponent,
+    PeoplesComponent,
+    SubscribeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
